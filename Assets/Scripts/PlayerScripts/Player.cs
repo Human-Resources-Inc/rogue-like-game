c98a7e81be9d18e05aa403cf973925fcd5aaa6d0
mili <mili@shihoya.ai>
2022-03-23 16:40:47 +0300

    Базовый функционал игры

---
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour, IDamageable
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public float movementSpeed = 5f;
+    public Rigidbody2D rigidbody2D;
+    public List<PowerUps> activePowerUps;
+    public Camera cam;
+
+    private Vector2 playerPosition;
+    private Vector2 mousePosition;
+
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private Texture2D playerCursor;
+    [SerializeField]
+    private Rigidbody2D firePoint;
+
+    
+    private void Start()
+    {
+        // Устанавливаем здоровье игрока и курсор
+        currentHealth = maxHealth;
+        Cursor.SetCursor(playerCursor, new Vector2(10, 10), CursorMode.Auto);
+    }
+
+    private void Update()
+    {
+        playerPosition.x = Input.GetAxisRaw("Horizontal");
+        playerPosition.y = Input.GetAxisRaw("Vertical");
+
+        // Необходима для смены анимации с idle на walking
+        bool magnitude = playerPosition.sqrMagnitude > 0 || playerPosition.magnitude < 0 ? true : false;
+        animator.SetBool("isMoving", magnitude);
+
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        // Поворачиваем игрока в зависимости в какую сторону он направляется
+        if (playerPosition.x < 0)
+            spriteRenderer.flipX = true;
+        else
+            spriteRenderer.flipX = false;
+
+        // Обновляем позицию игрока
+        rigidbody2D.MovePosition(rigidbody2D.position + playerPosition * movementSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDirection = mousePosition - rigidbody2D.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        firePoint.rotation = angle;
+        firePoint.transform.position = rigidbody2D.transform.position + new Vector3(0, .8f, 0);
+    }
+
+    public void Damage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+            SceneManager.LoadScene("SampleScene");
+    }
+}
---
+++ Assets/Scripts/PowerUps.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUps : MonoBehaviour
+{
+    public string modifierName;
+    public int damageModifier;
+    public float speedModifier;
+    public int healthModifier;
+    public SpriteRenderer spriteRenderer;
+    public Collider2D collider;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.transform.GetComponent<Player>();
+        if (player != null)
+        {
+            player.activePowerUps.Add(gameObject.GetComponent<PowerUps>());
+            player.movementSpeed += speedModifier;
+            player.maxHealth += healthModifier;
+            spriteRenderer.sprite = null;
+            collider.enabled = false;
+        }
+    }
+}
